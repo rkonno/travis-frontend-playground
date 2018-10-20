f96dbed3d9177548f0ba8b53966730a09e5b1747
Ryota Konno <rkonno.rb@gmail.com>
2018-10-20 17:54:28 +0900

    Modify

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,35 @@
 const gulp = require('gulp');
 
-gulp.task('css', () => {
+gulp.task('styles', () => {
     const postcss       = require('gulp-postcss');
     const sourcemaps    = require('gulp-sourcemaps');
 
-    return gulp.src('css/*.css')
+    return gulp.src('./src/styles/*.css')
         .pipe( sourcemaps.init() )
         .pipe( postcss([
             require('postcss-preset-env'),
             require('cssnano')
         ]) )
-        .pipe( sourcemaps.write('maps/') )
-        .pipe( gulp.dest('builds/') )
+        .pipe( sourcemaps.write('.') )
+        .pipe( gulp.dest('./dist/assets/styles') )
 });
+
+gulp.task('clean', () => {
+    const del = require('del');
+    return del(['./dist', './*.zip'], { force:true });
+});
+
+gulp.task('zip', () => {
+    const zip = require('gulp-zip');
+    return gulp.src('./dist')
+        .pipe(zip('archive.zip'))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('build',
+    gulp.series(
+        'clean',
+        'styles',
+        'zip'
+    )
+);
